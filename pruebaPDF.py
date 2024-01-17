0a3357d49a71607ffc5faaccd9a5870a9305adbf
Alex <alexarayam4@gmail.com>
2024-01-17 13:38:48 -0300

    función procesarPDF actualizada
    Toma y añade texto de imagen y texto en una sola pagina, pero no en el orden real, sino mas bien primero el texto de imágenes y luego los textos.

--- pruebaPDF.py
+++ pruebaPDF.py
@@ -24,7 +24,7 @@ def extraerTextoImagen(imagen):
         imagen_preprocesada = np.array(imagen_pil)
 
         texto = pytesseract.image_to_string(imagen_preprocesada, lang='spa')
-        return texto
+        return texto.strip()
     
     except Exception as e:
         # Manejar cualquier excepción que ocurra durante el procesamiento de la imagen
@@ -42,6 +42,8 @@ def procesarPDF(pdf_ruta, archivo_salida):
         pagina = doc[num_pagina]
         imagenes = pagina.get_images(full=True)
 
+        texto_pagina = ""
+
         for num_img, info_img in enumerate(imagenes):
             indice_img = info_img[0]
             img = doc.extract_image(indice_img)
@@ -51,8 +53,7 @@ def procesarPDF(pdf_ruta, archivo_salida):
             texto_imagen = extraerTextoImagen(np.array(imagen))
 
             if isinstance(texto_imagen, str) and texto_imagen.strip():
-                nueva_pagina = nuevo_doc.new_page(width=pagina.rect.width, height=pagina.rect.height)
-                nueva_pagina.insert_text((50, 50), texto_imagen)
+                texto_pagina += texto_imagen + "\n"
 
         # se recorre cada bloque de texto en la página y se agrega al nuevo documento PDF
         for bloque in pagina.get_text("blocks"):
@@ -64,47 +65,11 @@ def procesarPDF(pdf_ruta, archivo_salida):
                 continue
 
             if isinstance(texto_bloque, str) and texto_bloque.strip():
-                nueva_pagina = nuevo_doc.new_page(width=pagina.rect.width, height=pagina.rect.height)
-                nueva_pagina.insert_text((50, 50), texto_bloque)
+                texto_pagina += texto_bloque + "\n"
 
-    nuevo_doc.save(archivo_salida)
+        nueva_pagina = nuevo_doc.new_page(width=pagina.rect.width, height=pagina.rect.height)
+        nueva_pagina.insert_text((50, 50), texto_pagina)
 
-# procesar los documentos WORD con imagenes para convertirlos en documentos PDF de texto
-def procesarWord(word_ruta, archivo_salida):
-
-    # crear un nuevo documento PDF
-    nuevo_doc = fitz.open()
-
-    # se abre el documento word (.docx)
-    doc = Document(word_ruta)
-
-    # recorre cada imagen en el documento Word
-    for rel in doc.part.rels:
-        if "image" in doc.part.rels[rel].target_ref:
-            image_blob = doc.part.rels[rel].target_part.blob
-            imagen = Image.open(io.BytesIO(image_blob))
-            texto_imagen = extraerTextoImagen(np.array(imagen))
-
-            # insertar texto extraído de la imagen en el nuevo documento PDF
-            if texto_imagen.strip():
-                nueva_pagina = nuevo_doc.new_page(width=595, height=842)  # A4 size
-                nueva_pagina.insert_text((50, 50), texto_imagen)
-
-    # se recorre cada párrafo en el documento word
-    for para in doc.paragraphs:
-        
-        # elimina espacios en blanco al inicio y al final
-        texto = para.text.strip()
-
-        # se saltan los parrafos vacios
-        if not texto:
-            continue
-
-        # inserta texto en el nuevo documento PDF
-        nueva_pagina = nuevo_doc.new_page(width=595, height=842)  # A4 size
-        nueva_pagina.insert_text((50, 50), texto)
-
-    # guarda el nuevo documento PDF
     nuevo_doc.save(archivo_salida)
 
 # HACER FUNCION
@@ -123,10 +88,15 @@ for archivo in os.listdir(directorio_entrada):
         archivo_salida = os.path.join(directorio_salida, f"convertido_{archivo_sin_extension}.pdf")
         procesarPDF(archivo_entrada, archivo_salida)
     elif archivo.endswith(".docx"):
+        '''
         archivo_entrada = os.path.join(directorio_entrada, archivo)
         archivo_sin_extension, _ = os.path.splitext(archivo)
         archivo_salida = os.path.join(directorio_salida, f"convertido_{archivo_sin_extension}.pdf")
         procesarWord(archivo_entrada, archivo_salida)
+        '''
+                
+        print("documento no es PDF")
+
 
 print("DOCUMENTOS CONVERTIDOS A TEXTO OK")
 
